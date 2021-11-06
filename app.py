3b8cb6bbb1fc7b9cabe2705bc47161d1862b1523
Kninja99 <kcdamschen@yahoo.com>
2021-11-06 16:11:47 -0700

    updated the styles for workout page
    *updated the base style for workouts

--- app.py
+++ app.py
@@ -8,10 +8,18 @@ def home():
     # serves up the main page
     return render_template("index.html")
 
+
 @app.route("/user_form")
 def user_form():
     # serves page to get user info
     return render_template("user_form.html")
+
+
+@app.route("/workout")
+def workout():
+    # serves page to get user info
+    return render_template("workout.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
