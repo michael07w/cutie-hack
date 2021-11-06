8ec3738c38cf7ea9f351f55a15e570f2db63fdeb
Kninja99 <kcdamschen@yahoo.com>
2021-11-06 10:36:03 -0700

    Implemented the logic for a timer
    * added timer logic in timer.js
    * added scripts from js folder

---
+++ static/JS/timer.js
@@ -0,0 +1,35 @@
+class Timer {
+  constructor() {
+    this.time = 0;
+  }
+
+  /**
+   * This function will set the timer based off of difficulty setting
+   * @param {number} difficulty
+   */
+  setTime(difficulty) {
+    // difficulty settings 1-3
+    // setting time as in secounds
+    if (difficulty === 1) {
+      this.time = 20;
+    } else if (difficulty === 2) {
+      this.time = 30;
+    } else {
+      this.time = 45;
+    }
+  }
+
+  // tick needs another function call in app in order to run correctly
+  // and update the time
+
+  /**
+   * this function will tick the timer down to zero
+   */
+  tick() {
+    if (this.time > 0) {
+      this.time--;
+    } else {
+      console.log('time is done');
+    }
+  }
+}
