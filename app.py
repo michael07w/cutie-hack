5e8d2fc260bc98ffbf3bd917eceeb694cc3e0a6c
Kninja99 <kcdamschen@yahoo.com>
2021-11-06 09:15:12 -0700

    Added Base Structure
    added base structure for flask development

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    # serves up the main page
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
