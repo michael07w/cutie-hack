2421ef9686bc11799bd05500b8b9caf162c293f3
Michael Wessels <michaelwessels207@gmail.com>
2021-11-06 12:39:44 -0700

    Add functionality to redirect user to user_form.html upon button press.

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ def home():
     # serves up the main page
     return render_template("index.html")
 
+@app.route("/user_form")
+def user_form():
+    # serves page to get user info
+    return render_template("user_form.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
