a36aa1712a09dfcfe20e24e97b6fc5e4fb64999d
Michael Wessels <michaelwessels207@gmail.com>
2021-11-06 18:15:32 -0700

    Implemented logic to iterate through workouts

--- static/JS/app.js
+++ static/JS/app.js
@@ -1,5 +1,63 @@
-function runApp(userWeight, userHeight, userAge, userDifficulty) {
-  const testUser = new User(userWeight, userHeight, userAge, userDifficulty);
+// Below code must run after user submits their info on user_form.html
+function runApp(weight, height, age, difficulty) {
+    // Instantiate user
+    const athlete = new User(weight, height, age, difficulty);
+    // Create Workout List
+    workoutList = createWorkoutList(athlete);
+    // Process Workout
+    processWorkout(workoutList);
+}
 
-  console.log(testUser);
+// Generate Workouts
+function createWorkoutList (ourAthlete) {
+    let firstWorkout, secondWorkout, thirdWorkout;
+    
+    if (ourAthlete.difficulty === 1) {
+        firstWorkout = new Workout("Lunges");
+        secondWorkout = new Workout("High Knees");
+        thirdWorkout = new Workout("Squats");
+    
+    }
+    else if (ourAthlete.difficulty === 2) {
+        firstWorkout = new Workout("Plank-Ups");
+        secondWorkout = new Workout("Crunches");
+        thirdWorkout = new Workout("Mountain Climbers");
+    }
+    else {
+        firstWorkout = new Workout("Russian Twists");
+        secondWorkout = new Workout("Jump Squats");
+        thirdWorkout = new Workout("Push-Ups");
+    }
+    
+    // Add workouts to workoutList
+    let workoutList = new WorkoutDoublyLinkedList(firstWorkout);
+    workoutList.append(secondWorkout);
+    workoutList.append(thirdWorkout);
+
+    return workoutList;
+}
+
+function processWorkout() {
+    // get first workout in list
+    let currentWorkout = workoutList.head;
+    while (currentWorkout !== null) {
+        
+        /**
+        // create workout timer
+        workoutTimer = new Timer();
+        workoutTimer.setTime(testUser.difficulty);
+        */
+        // start workout timer
+        /** Code goes here. */
+
+        // tally calories burned
+        //testUser.calcCalories();
+        
+        // get next workout in list
+        console.log(currentWorkout);
+        currentWorkout = currentWorkout.next;
+    }
+
+    // finished workout, display stats
+    /** Code goes here. */
 }
