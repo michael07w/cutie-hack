b1612679af1fda5f62924aef49364555d3f51ae3
Kninja99 <kcdamschen@yahoo.com>
2021-12-24 21:43:29 -0800

    connected api to front end
    basic api call to backend to bring info to front end using states and hooks

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,26 +1,40 @@
-import logo from './logo.svg';
-import './css/main.css'
-import './css/workout.css'
+import React, { useEffect , useState} from 'react';
+import './css/main.css';
+import './css/workout.css';
 import Example from './components/Example';
-import Timer from './components/Timer'
+import Timer from './components/Timer';
 import ControlPanel from './components/ControlPanel';
-
-
 function App() {
+  // the generated workout state of the page
+  const [workout,setWorkout] = useState([{}])
+  // fetching generated workout into the state
+  useEffect(() => {
+    fetch("/generate_workout/easy").then(
+      res => res.json()).then(
+        randomWorkout => {
+          setWorkout(randomWorkout['workouts'])
+          console.log(randomWorkout['workouts'])
+        }
+      )
+  },[])
+
   return (
     <div className="App">
       <header>
         <a href="/">
           <img src="/static/images/clockLogo.png" alt="Clock Logo" />
-          <h1><span class="white-highlight">HITT</span> THE CLOCK</h1>
+          <h1>
+            <span class="white-highlight">HITT</span> THE CLOCK
+          </h1>
         </a>
       </header>
-      <h2 className= 'workout-name'>Pushups</h2>
+      <h2 className="workout-name">{workout[0]['workout']}</h2>
       <Example></Example>
       <Timer></Timer>
       <ControlPanel></ControlPanel>
     </div>
   );
 }
+
 
 export default App;
